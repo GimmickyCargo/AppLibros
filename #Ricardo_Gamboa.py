770f1c0eac9bfd7b63e78205e80f998d8711441b
GimmickyCargo <ric.gamboa@duocuc.cl>
2024-06-26 09:37:28 -0400

    Add files via upload

---
+++ #Ricardo_Gamboa.py
@@ -0,0 +1,46 @@
+#Aplicacion par Libreria
+AppLibros = ("AppLibros")
+
+Libro1 = ("50 sombras de Python","Juan Carlos","No Ficcion","25.000")
+Libro2 = ("La vida Otaku","Rodrigo Jimenes","Ficcion","30.000")
+Libro3 = ("GitHub para Novatos","Oscar de Astora","Ciencia","35.000")
+Libro4 = ("Recetas para Cafe","Maria Gomez","No Ficcion","20.000")
+Libro5 = ("Micro-Organismos","Cataliva Osses","Ciencia","40.000")
+Libro6 = ("Tragones y Mazmorras","Aillen Cordero","Ficcion","45.000")
+
+Generos1 = ("Ficcion")
+Generos2 = ("No Ficcion")
+Generos3 = ("Ciencia")
+
+
+1    ("Registrar libro"):
+2    ("Listar todos los libros"):
+3    ("Registrar venta"):
+4    ("Imprimir reporte de ventas"):
+5    ("Generar txt"):
+6    ("Salir del Programa"):
+        
+        ("Titulo","Autor","Genero","Precio")
+        
+        ("50 sombras de Python","Juan Carlos","No Ficcion","25.000")
+        ("La vida Otaku","Rodrigo Jimenes","Ficcion","30.000")
+        ("GitHub para Novatos","Oscar de Astora","Ciencia","35.000")
+        ("Recetas para Cafe","Maria Gomez","No Ficcion","20.000")
+        ("Micro-Organismos","Cataliva Osses","Ciencia","40.000")
+        ("Tragones y Mazmorras","Aillen Cordero","Ficcion","45.000")
+        
+        print("Registrar libro")
+        
+        ("Título del libro","Cantidad vendida","precio por unidad","precio final")
+        
+        ("50 sombras de Python","2";"25.000","50.000")
+        ("La vida Otaku","1";"30.000","30.000")
+        ("GitHub para Novatos","3","35.000","105.000")
+        ("Recetas para Cafe","1","20.000","20.000")
+        ("Micro-Organismos","2","40.000","80.000")
+        ("Tragones y Mazmorras","2","45.000","90.000")
+        
+        ("Ficcion","No Ficcion","Ciencia")
+        
+        opcion:1 = ("imprimir todos") 
+        opcion:2 = ("imprimir genero")
